c3a529d672dea1e9b741b3af5d95d4b0da079cb8
edrassimoes <edrassimoes@gmail.com>
2026-03-05 23:59:51 -0300

    feat(notification): implementa NotificationService para envio de notificações

---
+++ src/main/java/br/com/edras/picpaysimplificado/service/AuthorizationService.java
@@ -0,0 +1,34 @@
+package br.com.edras.picpaysimplificado.service;
+
+import br.com.edras.picpaysimplificado.client.AuthorizerClient;
+import br.com.edras.picpaysimplificado.client.AuthorizerResponse;
+import br.com.edras.picpaysimplificado.domain.enums.TransactionStatus;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService {
+
+    private final AuthorizerClient authorizerClient;
+
+    public AuthorizationService(AuthorizerClient authorizerClient) {
+        this.authorizerClient = authorizerClient;
+    }
+
+    @CircuitBreaker(name = "authorizerService", fallbackMethod = "authorizeFallback")
+    public TransactionStatus authorize() {
+
+        AuthorizerResponse response = authorizerClient.authorize();
+
+        if (response.getData().isAuthorization()) {
+            return TransactionStatus.AUTHORIZED;
+        }
+
+        return TransactionStatus.FAILED;
+    }
+
+    public TransactionStatus authorizeFallback(Throwable t) {
+        return TransactionStatus.PENDING;
+    }
+
+}
---
+++ src/main/java/br/com/edras/picpaysimplificado/service/NotificationService.java
@@ -0,0 +1,33 @@
+package br.com.edras.picpaysimplificado.service;
+
+import br.com.edras.picpaysimplificado.client.NotificationClient;
+import br.com.edras.picpaysimplificado.client.NotificationRequest;
+import br.com.edras.picpaysimplificado.domain.Transaction;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+    private final NotificationClient notificationClient;
+
+    public NotificationService(NotificationClient notificationClient) {
+        this.notificationClient = notificationClient;
+    }
+
+    public void sendTransactionNotification(Transaction transaction) {
+        try {
+
+            String message = String.format(
+                    "Transação de R$ %.2f realizada com sucesso",
+                    transaction.getAmount()
+            );
+
+            NotificationRequest request = new NotificationRequest(message);
+            notificationClient.notify(request);
+
+        } catch (Exception e) {
+            // log futuramente
+        }
+    }
+
+}
