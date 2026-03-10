072bd46bc6ac0ce3f4d7c863019927994ba40131
edrassimoes <edrassimoes@gmail.com>
2026-03-10 00:20:21 -0300

    refactor(transaction): adiciona suporte à idempotência e refatora fluxo de transferência

--- src/main/java/br/com/edras/picpaysimplificado/service/TransactionService.java
+++ src/main/java/br/com/edras/picpaysimplificado/service/TransactionService.java
@@ -14,6 +14,9 @@ import br.com.edras.picpaysimplificado.exception.transaction.SameUserTransaction
 import br.com.edras.picpaysimplificado.exception.transaction.TransactionNotAuthorizedException;
 import br.com.edras.picpaysimplificado.exception.transaction.TransactionNotFoundException;
 import br.com.edras.picpaysimplificado.exception.user.UserNotFoundException;
+import br.com.edras.picpaysimplificado.idempotency.IdempotencyKey;
+import br.com.edras.picpaysimplificado.idempotency.IdempotencyService;
+import br.com.edras.picpaysimplificado.idempotency.enums.RequestStatus;
 import br.com.edras.picpaysimplificado.repository.TransactionRepository;
 import br.com.edras.picpaysimplificado.repository.UserRepository;
 import jakarta.transaction.Transactional;
@@ -22,6 +25,8 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -33,14 +38,18 @@ public class TransactionService {
     private final UserRepository userRepository;
     private final WalletService walletService;
     private final AuthorizationService authorizationService;
+    private final IdempotencyService idempotencyService;
     private final ApplicationEventPublisher eventPublisher;
+    private final ObjectMapper objectMapper;
 
-    public TransactionService(TransactionRepository transactionRepository, UserRepository userRepository, WalletService walletService, AuthorizationService authorizationService, ApplicationEventPublisher eventPublisher) {
+    public TransactionService(TransactionRepository transactionRepository, UserRepository userRepository, WalletService walletService, AuthorizationService authorizationService, IdempotencyService idempotencyService, ApplicationEventPublisher eventPublisher, ObjectMapper objectMapper) {
         this.transactionRepository = transactionRepository;
         this.userRepository = userRepository;
         this.walletService = walletService;
         this.authorizationService = authorizationService;
+        this.idempotencyService = idempotencyService;
         this.eventPublisher = eventPublisher;
+        this.objectMapper = objectMapper;
     }
 
     private void merchantDeposit(MerchantUser merchantUser, Double amount) {
@@ -49,35 +58,72 @@ public class TransactionService {
         walletService.createOrUpdateWallet(wallet);
     }
 
-    @Transactional
-    @Caching(evict = {
-            @CacheEvict(value = "transactions:user", key = "#dto.payerId"),
-            @CacheEvict(value = "transactions:user", key = "#dto.payeeId")
-    })
-    public TransactionResponseDTO transfer(TransactionRequestDTO dto) {
-
-        if (dto.getPayerId().equals(dto.getPayeeId())) {
-            throw new SameUserTransactionException();
-        }
-
-        User payer = userRepository.findById(dto.getPayerId())
-                .orElseThrow(() -> new UserNotFoundException(dto.getPayerId()));
+    private User getPayer(Long payerId) {
+        User payer = userRepository.findById(payerId)
+                .orElseThrow(() -> new UserNotFoundException(payerId));
 
         if (payer.getUserType() == UserType.MERCHANT) {
             throw new MerchantCannotTransferException(payer.getId());
         }
 
-        User payee = userRepository.findById(dto.getPayeeId())
-                .orElseThrow(() -> new UserNotFoundException(dto.getPayeeId()));
+        return payer;
+    }
 
-        // Cria a transação no banco.
+    private User getPayee(Long payeeId) {
+        return userRepository.findById(payeeId)
+                .orElseThrow(() -> new UserNotFoundException(payeeId));
+    }
+
+    private Transaction createTransaction(User payer, User payee, Double amount) {
         Transaction transaction = new Transaction();
-        transaction.setAmount(dto.getAmount());
+        transaction.setAmount(amount);
         transaction.setPayer(payer);
         transaction.setPayee(payee);
         transaction.setCreatedAt(LocalDateTime.now());
         transaction.setStatus(TransactionStatus.PENDING);
-        Transaction savedTransaction = transactionRepository.save(transaction);
+        return transactionRepository.save(transaction);
+    }
+
+    private void processTransfer(User payer, User payee, Double amount) {
+        walletService.withdraw(payer.getId(), amount);
+
+        if (payee instanceof MerchantUser) {
+            merchantDeposit((MerchantUser) payee, amount);
+        } else {
+            walletService.deposit(payee.getId(), amount);
+        }
+    }
+
+    @Transactional
+    @Caching(evict = {
+            @CacheEvict(value = "transactions:user", key = "#dto.payerId"),
+            @CacheEvict(value = "transactions:user", key = "#dto.payeeId")
+    })
+    public TransactionResponseDTO transfer(String key, TransactionRequestDTO dto) {
+
+        IdempotencyKey idempotencyKey = idempotencyService.createKey(key);
+
+        // Verifica o status da requisição
+        if (idempotencyKey.getStatus() == RequestStatus.COMPLETED && idempotencyKey.getResponse() != null) {
+            try {
+                return objectMapper.readValue(idempotencyKey.getResponse(), TransactionResponseDTO.class);
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to deserialize idempotent response", e);
+            }
+        }
+
+        if (idempotencyKey.getStatus() == RequestStatus.PROCESSING && idempotencyKey.getResponse() == null) {
+            throw new IllegalStateException("Request with this Idempotency-Key is already being processed");
+        }
+
+        if (dto.getPayerId().equals(dto.getPayeeId())) {
+            throw new SameUserTransactionException();
+        }
+
+        User payer = getPayer(dto.getPayerId());
+        User payee = getPayee(dto.getPayeeId());
+
+        Transaction savedTransaction = createTransaction(payer, payee, dto.getAmount());
 
         // API de autenticação
         TransactionStatus authStatus = authorizationService.authorize();
@@ -85,23 +131,29 @@ public class TransactionService {
         if (authStatus != TransactionStatus.AUTHORIZED) {
             savedTransaction.setStatus(authStatus);
             transactionRepository.save(savedTransaction);
+            idempotencyKey.setStatus(RequestStatus.COMPLETED);
+            idempotencyService.save(idempotencyKey);
             throw new TransactionNotAuthorizedException();
         }
 
-        // Realiza a transação.
-        walletService.withdraw(payer.getId(), dto.getAmount());
-
-        if (payee instanceof MerchantUser) {
-            merchantDeposit((MerchantUser) payee, dto.getAmount());
-        } else {
-            walletService.deposit(payee.getId(), dto.getAmount());
-        }
+        // Realiza a transação
+        processTransfer(payer, payee, dto.getAmount());
 
         savedTransaction.setStatus(TransactionStatus.COMPLETED);
         Transaction completedTransaction = transactionRepository.save(savedTransaction);
 
-        // Publica um evento -> API de notificação.
+        // Publica um evento -> API de notificação
         eventPublisher.publishEvent(new TransactionCompletedEvent(completedTransaction));
+
+        // Converte a resposta em JSON e salva no banco
+        try {
+            String jsonResponse = objectMapper.writeValueAsString(new TransactionResponseDTO(completedTransaction));
+            idempotencyKey.setResponse(jsonResponse);
+            idempotencyKey.setStatus(RequestStatus.COMPLETED);
+            idempotencyService.save(idempotencyKey);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to serialize idempotent response", e);
+        }
 
         return new TransactionResponseDTO(completedTransaction);
     }
